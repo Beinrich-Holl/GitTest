0b7d69a7aafd4fff6e9dd364b6213e9027668f74
beinrichholl <m.amirhossein1379@gmail.com>
2023-03-24 13:01:20 +0330

    Initial Commit

---
+++ gittest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by Apple on 3/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
